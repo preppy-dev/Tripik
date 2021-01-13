5207cd22983be63a46338fed69337dc1aef81f00
ZuluLab <zululab@MacBook-Pro-de-ZuluLab.local>
2021-01-13 16:15:04 -0300

    Updates

---
+++ precache-manifest.020fb988fe044634dbf8cf2a63ab0b42.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54889741690a8d48054896daab953fd",
+    "url": "/Tripik/index.html"
+  },
+  {
+    "revision": "2ccd8d15556fcd97ab39",
+    "url": "/Tripik/static/css/main.5a92ca9d.chunk.css"
+  },
+  {
+    "revision": "db07b3d36a7c63bab094",
+    "url": "/Tripik/static/js/2.0239f778.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tripik/static/js/2.0239f778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccd8d15556fcd97ab39",
+    "url": "/Tripik/static/js/main.b111a0c1.chunk.js"
+  },
+  {
+    "revision": "7b3e3bc130f1bdb87c71",
+    "url": "/Tripik/static/js/runtime-main.c97a55a7.js"
+  },
+  {
+    "revision": "38d5c15a11c2c8d0fc977cbeb8bbacb7",
+    "url": "/Tripik/static/media/Tripik.38d5c15a.png"
+  },
+  {
+    "revision": "9d8d68e120a0b1f7dc4279f69cece8ae",
+    "url": "/Tripik/static/media/Twitter.9d8d68e1.svg"
+  },
+  {
+    "revision": "67fc901fca1d7ebec058bea0613a043a",
+    "url": "/Tripik/static/media/bghome1.67fc901f.png"
+  },
+  {
+    "revision": "1f186e9144ed10301e74c346b8891627",
+    "url": "/Tripik/static/media/bghome2.1f186e91.png"
+  },
+  {
+    "revision": "38d6b45abb6a26e6e9dc4608037f9fc1",
+    "url": "/Tripik/static/media/bghome3.38d6b45a.png"
+  },
+  {
+    "revision": "899bb608d6216c03bfd601755155eb54",
+    "url": "/Tripik/static/media/blogexample.899bb608.jpg"
+  },
+  {
+    "revision": "6d73e331533ea77bdb877ada35ee47d9",
+    "url": "/Tripik/static/media/boxabout.6d73e331.png"
+  },
+  {
+    "revision": "27fdf0743dbd8d1fe701a9aca346cfbb",
+    "url": "/Tripik/static/media/fb.27fdf074.svg"
+  },
+  {
+    "revision": "5dc63a999843df943f07d063f1768f67",
+    "url": "/Tripik/static/media/instagram.5dc63a99.svg"
+  },
+  {
+    "revision": "5d2a137480e0387106203eb191426b38",
+    "url": "/Tripik/static/media/learnimg.5d2a1374.png"
+  }
+]);
